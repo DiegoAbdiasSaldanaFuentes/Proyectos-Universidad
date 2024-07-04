482928b0dc0bd2bd6d2ae6f8cdd16192f73237f6
DERPVTRV <derpvtrvtuber@gmail.com>
2024-07-03 22:48:17 -0400

    trghj

--- Analizando la Hemoglobina/hemoglobina.py
+++ Analizando la Hemoglobina/hemoglobina.py
@@ -1,10 +1,8 @@
 
-edad= 12
-nivel_hemoglobina= 16
-escala_edad="1"
-genero="1"
-
-
+edad= int(input("introduzca su edad: "))
+nivel_hemoglobina= float(input("introduzca su nivel de hemoglobina: "))
+escala_edad= str(input("introdusca su escala de edad 0 para mes 1 para año: "))
+genero= str(input("introdusca su genero 0 para hombre y 1 para mujer: "))
 
 if escala_edad == "0":
         if  edad <= 1:
@@ -20,15 +18,13 @@ if  escala_edad == "1":
             minimo,maximo= 12.6, 15.5
         elif edad <=15:
             minimo,maximo= 13,15.5
-#hombre
-if genero == "0" and escala_edad== 1:
-        if edad > 15:
-            minimo,maximo= 12,16
-#mujer
-if genero == "1" and escala_edad ==1:
-        if edad > 15:
-            minimo,maximo= 14 ,18 
-
+        else:
+            if genero == "0":#Hombre
+                if edad > 15:
+                    minimo,maximo= 12,16
+            elif genero == "1":#Mujer
+                if edad > 15:
+                    minimo,maximo= 14 ,18 
 
 if nivel_hemoglobina <= minimo:
     print ("negativo")
